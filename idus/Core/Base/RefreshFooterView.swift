88beb479000a9328f9d26328a264a591ce192103
seungjin <seungjin429@naver.com>
2019-12-04 19:40:57 +0900

    refresh footer view 리소스 추가

---
+++ idus/Core/Base/RefreshFooterView.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshFooterView.swift
+//  idus
+//
+//  Created by Seungjin on 04/12/2019.
+//  Copyright © 2019 Jinnify. All rights reserved.
+//
+
+import UIKit
+
+class RefreshFooterView: UICollectionReusableView {
+        
+}
