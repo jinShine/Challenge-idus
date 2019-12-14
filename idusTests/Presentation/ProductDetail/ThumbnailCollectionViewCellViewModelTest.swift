2f347b7e4ecf605a195be072f574b8dce8799461
jinnify <seungjin429@gmail.com>
2019-12-15 01:26:22 +0900

    ThumbnailCollectionViewCellViewModel Test 작성

--- idusTests/Presentation/ProductDetail/ThumbnailCollectionViewCellViewModelTest.swift
+++ idusTests/Presentation/ProductDetail/ThumbnailCollectionViewCellViewModelTest.swift
@@ -12,13 +12,29 @@ import XCTest
 
 class ThumbnailCollectionViewCellViewModelTest: XCTestCase {
   
+  var thumbnailList: [String]?
+  
   override func setUp() {
-    
-//    ThumbnailCollectionViewCellViewModel(thumbnailList: <#T##[String]#>)
+    expectation { expectation in
+      Morker.productDetail(stub: .product_detail) { product in
+        self.thumbnailList = product.thumbnailList
+          .components(separatedBy: "#")
+          .map { $0.replacingOccurrences(of: "_320", with: "_720") }
+        expectation.fulfill()
+      }
+    }
   }
   
-  override func tearDown() {
-    
+  override func tearDown() { }
+  
+  func test_thumbnailList_Count() {
+    let vm = ThumbnailCollectionViewCellViewModel(thumbnailList: thumbnailList!)
+    XCTAssertTrue(vm.thumbnailList.count > 0)
+  }
+  
+  func test_NumberofSections() {
+    let vm = ThumbnailCollectionViewCellViewModel(thumbnailList: thumbnailList!)
+    XCTAssertEqual(vm.numberOfSections(), 1)
   }
   
 }
