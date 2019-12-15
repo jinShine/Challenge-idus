3ed446997debe08a22e94fa895b6ede65c81c0d9
jinnify <seungjin429@gmail.com>
2019-12-15 21:43:28 +0900

    용량으로 인해 커스텀 폰트 제거

--- idus/Utility/Theme/Font.swift
+++ idus/Utility/Theme/Font.swift
@@ -19,49 +19,9 @@ struct Font {
     return UIFont(name: "NotoSansCJKkr-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
   }
   
-  func notoSansThin(size: CGFloat) -> UIFont {
-    return UIFont(name: "NotoSansCJKkr-Thin", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func notoSansRegular(size: CGFloat) -> UIFont {
-    return UIFont(name: "NotoSansCJKkr-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func notoSansLight(size: CGFloat) -> UIFont {
-    return UIFont(name: "NotoSansCJKkr-Light", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func notoSansDemiLight(size: CGFloat) -> UIFont {
-    return UIFont(name: "NotoSansCJKkr-DemiLight", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func notoSansMedium(size: CGFloat) -> UIFont {
-    return UIFont(name: "NotoSansCJKkr-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
   //MARK:- SF Pro
   func sfProTextHeavy(size: CGFloat) -> UIFont {
     return UIFont(name: "SFProText-Heavy", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func sfProTextMedium(size: CGFloat) -> UIFont {
-    return UIFont(name: "SFProText-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func sfProTextBold(size: CGFloat) -> UIFont {
-    return UIFont(name: "SFProText-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func sfProTextLight(size: CGFloat) -> UIFont {
-    return UIFont(name: "SFProText-Light", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func sfProTextRegular(size: CGFloat) -> UIFont {
-    return UIFont(name: "SFProText-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
-  }
-  
-  func sfProTextSemibold(size: CGFloat) -> UIFont {
-    return UIFont(name: "SFProText-Semibold", size: size) ?? UIFont.systemFont(ofSize: size)
   }
   
   //MARK:- Apple SD GothicNeo
